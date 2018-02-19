08fdcc7a2cd029332ccaf613b74a5de41cc64433
Nadir Pervez <nadirpak95@yahoo.com>
2018-02-19 23:09:05 +0200

    no message

---
+++ Assets/gear-vr-leap/Scripts/InputFieldControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class InputFieldControl : MonoBehaviour
+{
+	NetworkManager manager;
+	InputField inputText;
+	Text placeHolder;
+	// Use this for initialization
+	void Start()
+	{
+		GetNecessaryComponents();
+		placeHolder.text = LoadLastIP();
+		manager.networkAddress = placeHolder.text;
+	}
+
+	public void AsTyping()
+	{
+		inputText.textComponent.text = inputText.text;
+		manager.networkAddress = inputText.text;
+	}
+
+	void GetNecessaryComponents()
+	{
+		inputText = GetComponent<InputField>();
+		placeHolder = transform.GetChild(0).GetComponent<Text>();
+		manager = GameObject.Find("Controller").GetComponent<NetworkManager>();
+	}
+
+	public void AfterTyping()
+	{
+		PlayerPrefs.SetString("IP", inputText.text);
+		manager.networkAddress = inputText.text;
+	}
+
+	string LoadLastIP()
+	{
+		string lastIP = PlayerPrefs.GetString("IP", "IP Address: ");
+		return lastIP;
+	}
+}
---
+++ Assets/gear-vr-leap/Scripts/OverridenNetworkDiscovery.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class OverridenNetworkDiscovery : NetworkDiscovery
+{
+    public GameObject leapPrefab;
+
+    void Start()
+    {
+        startClient();
+    }
+
+    public void startClient()
+    {
+        Initialize();
+        StartAsClient();
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        Debug.Log("I work");
+        Debug.Log(fromAddress);
+        NetworkManager.singleton.networkAddress = fromAddress;
+        NetworkManager.singleton.StartClient();
+        StopBroadcast();
+        
+    }
+}
